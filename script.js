49fd4255b11858bb15611dd2669d9820cb3beeb1
Gemcila <ggemcila@gmail.com>
2021-02-04 17:53:00 -0800

    Defined "const"( infos we need to display in the webpage)

---
+++ script.js
@@ -0,0 +1,4 @@
+ const eventKey = document.getElementById('key');
+ const eventCode = document.getElementById('code');
+ const eventWhich = document.getElementById('which');
+ const title = document.querySelector('h1');
