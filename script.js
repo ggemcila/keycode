59c080755d20af82283507ba4e4227aa025ac375
Gemcila <ggemcila@gmail.com>
2021-02-04 17:53:59 -0800

    Started defining addEventListener

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
  const eventCode = document.getElementById('code');
  const eventWhich = document.getElementById('which');
  const title = document.querySelector('h1');
+ 
+ window.addEventListener("keydown", (event) => {
+   
